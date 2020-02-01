620f6145c96cf9f174b490dbd29ac9ccb51da745
Zac Uslianer <zacuslianer@gmail.com>
2020-02-01 23:39:08 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -18,13 +18,19 @@ def division(num1, num2)
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  puts product
+  product
 end
 
 def modulo(num1, num2)
-
+  remainder = num1 % num2
+  puts remainder
+  remainder
 end
 
 def square_root(num)
-
+  root = Math.sqrt(num)
+  puts root 
+  root
 end
